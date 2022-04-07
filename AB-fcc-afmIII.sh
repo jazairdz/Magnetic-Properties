12147e9b72a6eebcb2c33a4c718c5176d83723ff
jazairdz <jazairdz@gmail.com>
2022-04-07 02:10:45 +0100

    Update AB-fcc-afmIII.sh

--- AB-fcc-afmIII.sh
+++ AB-fcc-afmIII.sh
@@ -28,19 +28,13 @@ P
 cp *super.struct $wdir.struct-P1x2x1
 cp $wdir.struct-P1x2x1 $wdir.struct-P1x2x1-2
 
-
 # create the AFM structure
-
 sed -e '7 s/Ni/Ni1/' -e '13 s/Ni/Ni2/' -e '19 s/Ni/Ni1/' -e '25 s/Ni/Ni1/' \
 -e '31 s/Ni/Ni2/' -e '37 s/Ni/Ni2/' -e '43 s/Ni/Ni1/' -e '49 s/Ni/Ni2/' \
 $wdir.struct-P1x2x1-2 > $wdir.struct
-
 rm $wdir.struct-P1x2x1-2
-
 cp $wdir.struct $wdir.struct-afmIII-ws
-
 sgroup -wi $wdir.struct -wo $wdir.struct_sgroup
-
 cp $wdir.struct_sgroup $wdir.struct-afmIII
-
 cp $wdir.struct-cubic-F $wdir.struct
+
